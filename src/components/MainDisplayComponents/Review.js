d9008f5582a6465ce1f3d747ce52fc86be360d68
labeesley <cluelessclaptrp@gmail.com>
2023-06-23 21:04:54 -0400

    added Main Display, ReviewFeed, and Review components

---
+++ src/components/MainDisplayComponents/Review.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Review = () => {
+  return (
+    <div>
+      <h2>I am Review</h2>
+      <div>
+        <h3>(Left side)</h3>
+        <h3>Practitioner: (STATE)</h3>
+        <h3>Practice Name: (STATE)</h3>
+        <h3>Address: (STATE)</h3>
+        <h3>Link/Website: (STATE)</h3>
+      </div>
+      <div>
+        <h3>(Right Side)</h3>
+        <h3>Star Rating (STATE)</h3>
+        <h3>Specialty: (STATE)</h3>
+        <button>Add Review</button> 
+      </div>
+    </div>
+  )
+}
+
+export default Review;
