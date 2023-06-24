4279694cd02c8ea5053bcad61b7dd2cdfb0efec5
Sbrown2018 <sara.brown07.sb@gmail.com>
2023-06-23 17:39:45 -0700

    "overlay files"

---
+++ src/components/Overlay.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+
+import "../stylesheets/overlay.css";
+
+export function Overlay({isOpen, children}) {
+  return (
+    <Fragment>
+      <!--Renders a div if isOpen prop is true -->
+      {isOpen && (
+        <div className="overlay">
+          <!--Renders whatever you pass in to this component inside the parent div.-->
+          {children}
+        </div>
+      )}
+    </Fragment>
+  );
+}
+
+export default Overlay;
