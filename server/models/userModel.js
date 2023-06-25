2a91aa632de64cad3f7f35f8e8b9ff95c2283359
labeesley <cluelessclaptrp@gmail.com>
2023-06-25 18:51:44 -0400

    created db models

---
+++ server/models/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  pronouns: { type: String, required: true },
+  zipcode: { type: Number, required: true },
+  location: { type: String, required: true },
+  reviews: {},
+}) 
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
