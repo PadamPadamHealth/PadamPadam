2ddcef40d3fd89001c90fcef98bc3a9f62d0b851
labeesley <cluelessclaptrp@gmail.com>
2023-06-25 14:21:27 -0400

    index.js file configured
    Co-authored-by: Sara Brown <sara.brown07.sb@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
-// ReactDom.render(<App />, document.getElementById('root'))
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 )
 
 root.render (
-  <App />
+  <BrowserRouter>  
+    <App />
+  </BrowserRouter>
 )
